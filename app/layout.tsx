8d8c7060fc50589a2c944eebbad8c55393f36e05
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-19 14:03:30 +0300

    initial commit with implemented login

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { ThemeToggle } from "@/components/ui/themeToggle";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-red-500">Hello world</h1>
+      <ThemeToggle/>
+    </div>
+  );
+}
