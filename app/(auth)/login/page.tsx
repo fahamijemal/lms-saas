a6fd66bd9d462e2d44a2b110b4bd3756a22ddb16
Fahami Jemal <fahamijemal1@gmail.com>
2025-07-19 19:55:31 +0300

    implemented loggout and session

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,69 +1,18 @@
-'use client';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { authClient } from '@/lib/auth-client';
-import { error } from 'console';
-import {GithubIcon, Loader } from 'lucide-react';
-import { useTransition } from 'react';
-import { toast } from 'sonner';
+import { auth } from "@/lib/auth";
+import { LoginForm } from "./_components/LoginForm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
-  const [githubPending, startGithubTransition] = useTransition()
-  async function signInWithGithub() {
-    startGithubTransition(async () =>{
-      await authClient.signIn.social({
-        provider:'github',
-        callbackURL:"/",
-        fetchOptions:{
-          onSuccess:() =>{
-            toast.success("Signed in with Github, you will be redirected ...");
-          },
-          onError:(error) =>{
-            toast.error("Internal Server Error")
-          },
-        },
-    });  
-    });
+const LoginPage = async () => {
+  const session=await auth.api.getSession({
+    headers:await headers(),
+    
+  });
+  if(session){
+    return redirect("/")
   }
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle className='text-xl'>Welcome back!</CardTitle>
-        <CardDescription >Login with your Github Email Account</CardDescription>
-      </CardHeader>
-      <CardContent className='flex flex-col gap-4'> 
-        <Button 
-          disabled={githubPending}
-          onClick={signInWithGithub} 
-          className='w-full' 
-          variant="outline" >
-          {githubPending ? (
-            <>
-            <Loader className='size-4 animate-spin'/>
-            <span>Loading... </span>
-            </>
-          ):(
-            <>
-              <GithubIcon className='size-4'/>
-              Sign in with Github
-            </>
-          )}
-        </Button>
-        <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
-          <span className='relative z-10 bg-card px-2 text-muted-foreground'>Or continue with</span>
-        </div>
-
-        <div className='grip gap-3'>
-          <div className='grid gap-2'>
-            <Label htmlFor='email'>Email</Label>
-            <Input type='email' placeholder='m@example.com'/>
-          </div>
-          <Button>continue with Email</Button>
-        </div>
-      </CardContent>
-    </Card>
+    <LoginForm/>
   )
 }
 
